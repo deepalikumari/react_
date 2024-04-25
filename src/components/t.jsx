50790acdc0e5bfd12b7944d6e7a1638b4051eb8e
Deepali kumari <deepalikumari1234@gmail.com>
2024-04-25 10:52:04 +0530

    did changes

--- src/components/t.jsx
+++ src/components/t.jsx
@@ -1,13 +1,6 @@
+import { useState } from 'react';
 import './App.css';
-import React, { useState } from 'react';
-
-import Prgn1 from './components/prgn1';
-import Prgn2 from './components/prgn2';
-import Prgn3 from './components/prgn3';
-import Prgn4 from './components/prgn4';
-import Prgn5 from './components/prgn5';
 import Prgn6 from './components/prgn6';
-import Task from './components/Task';
 
 export const ToggleTheme = React.createContext()
 
@@ -22,13 +15,7 @@ function App() {
   return (
     <ToggleTheme.Provider value={state}>
       <button onClick={handleToggle}>Toggle</button>
-        <Prgn1/>
-        <Prgn2/>
-        <Prgn3 />
-        <Prgn4 />
-
-        <Prgn6 />
-        <Task />
+      <Prgn6/>
     </ToggleTheme.Provider>
   );
 }
